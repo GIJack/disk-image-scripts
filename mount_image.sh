2f0e65fefbd133717f7f422ba8bd2a94e8466c87
GI_Jack <GI_Jack@hackermail.com>
2020-08-13 16:41:44 -0400

    now works with multiple mounts. commands re-written list - now works. shows numbered list of mount locations mount - now picks first available loop device instead of hardcoded loop1 umount - now needs a number of loop device to unmount. uses numbers specified in list

--- mount_image.sh
+++ mount_image.sh
@@ -3,7 +3,8 @@
 #
 COMMAND="${1}"
 FILENAME="${2}"
-LOOP_DEV=loop1
+LOOP_DEV=$(losetup -f)
+PART_N=1
 MOUNT_POINT="${HOME}/mnt"
 ROOT_METHOD="sudo"
 
@@ -12,11 +13,16 @@ help_and_exit(){
 mount_image.sh:
 
 Mount and dismount raw disk images. assumes a single parition in the
-file.
+file. n is the number of the loop device, as shown in list. An image
+needs to be a paritioned disk image
 
-USAGE: mount_image.sh <command> <file.img>
+USAGE: mount_image.sh <command> [file.img|n]
 
 Commands: mount umount list
+
+    mount: mount_image mount <filename.img>
+    
+    umount: mount_image.sh umount <n>
 
 EOF
   exit 2
@@ -30,11 +36,14 @@ exit_with_error(){
   exit ${1}
 }
 
-as_root(){
+as_root() {
   # execute a command as root.
   case $ROOT_METHOD in
    sudo)
     sudo ${@}
+    ;;
+   pkexec)
+    pkexec ${@}
     ;;
    uid)
     ${@}
@@ -44,26 +53,44 @@ as_root(){
 
 _mount-img() {
   local -i local_exit=0
-  message "Mounting ${FILENAME} on /dev/${LOOP_DEV} on ${MOUNT_POINT}"
-  as_root losetup -P ${LOOP_DEV} "${FILENAME}"
-  local_exit+=${?}
-  as_root mount /dev/${LOOP_DEV}p1 ${MOUNT_POINT}
-  local_exit+=${?}
+  message "Mounting ${FILENAME} on ${LOOP_DEV} on ${MOUNT_POINT}"
+
+  as_root losetup -P ${LOOP_DEV} "${FILENAME}" || local_exit+=1
+  as_root mount ${LOOP_DEV}p${PART_N} ${MOUNT_POINT} || local_exit+=1
+
   return ${local_exit}
 }
 
 _umount-img() {
   local -i local_exit=0
-  message "UnMounting ${MOUNT_POINT} from /dev/${LOOP_DEV}"
-  as_root umount ${MOUNT_POINT}
-  local_exit+=${?}
-  as_root losetup -d /dev/${LOOP_DEV}
-  local_exit+=${?}
+  LOOP_DEV=/dev/loop${1}
+
+  message "UnMounting ${MOUNT_POINT} from ${LOOP_DEV}"
+
+  as_root umount ${MOUNT_POINT} || local_exit+=1
+  as_root losetup -d ${LOOP_DEV} || local_exit+=1
+
   return ${local_exit}
 }
 
-_list_mounts(){
-  exit_with_error 4 "not implemented yet"
+_list_mounts() {
+  local -i local_exit=0
+  local loop_names=( $(losetup -a |cut -d ":" -f 1) )
+  local loop_imgs=( $(losetup -a |cut -d ":" -f 3) )
+
+  local -i item=0
+  local n=${#loop_names[@]}
+  for ((i=0; $i < $n ; i+=1));do
+    j=$((${#item}-1))
+    loop_names[${i}]=${i:${j}:1}
+  done
+  
+  message "Mounted Images:"
+  local -i item=0
+  local n=${#loop_imgs[@]}
+  for ((i=0; $i < $n ; i+=1));do
+    echo "${loop_names[$i]} : ${loop_imgs[$i]}"
+  done
 }
 
 main() {
@@ -73,10 +100,11 @@ main() {
       _mount-img || exit_with_error 1 "Could not mount ${FILENAME}"
       ;;
     umount)
-      _umount-img || exit_with_error 1 "Could Not unmount ${MOUNT_POINT}"
+      [ -z ${FILENAME} ] && help_and_exit
+      _umount-img ${FILENAME} || exit_with_error 1 "Could Not unmount ${MOUNT_POINT}"
       ;;
     list)
-      list-mounts || exit_with_error 1 "Couldn't list mounts???"
+      _list_mounts || exit_with_error 1 "Couldn't list mounts???"
       ;;
     *)
       help_and_exit
