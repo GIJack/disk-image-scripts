df9ae204a8073ddef4706a764c841c44c5e7a1be
GI_Jack <GI_Jack@hackermail.com>
2021-05-30 12:45:16 -0400

    fixxed gen_cloud_template completion, added init-template

--- bash_completion/gen_cloud_template.bash
+++ bash_completion/gen_cloud_template.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-complete -W "help init-image update-image image-shell compile-template" gen_cloud_template.sh
+complete -W "help init-image init-template update-image image-shell compile-template" gen_cloud_template.sh
