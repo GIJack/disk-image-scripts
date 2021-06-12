368b3814ba077c55fd9ee251d09254836c0337e6
GI_Jack <GI_Jack@hackermail.com>
2021-06-12 18:27:19 -0400

    updated bash completion for subcommands. mount lists files in directory, umount has place holder

--- bash_completion/mount_image.bash
+++ bash_completion/mount_image.bash
@@ -1,3 +1,32 @@
 #!/usr/bin/bash
 
-complete -W "mount umount list" mount_image.sh
+_mount_image_completion() {
+  local cur prev
+  local output=""
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+
+  case ${COMP_CWORD} in
+    1)
+      COMPREPLY=( $(compgen -W "mount umount list" -- ${cur}) )
+      ;;
+    2)
+      case ${prev} in
+        mount)
+          output=($(ls *))
+          COMPREPLY=($(compgen -W "${output[*]}" -- ${cur}))
+          ;;
+        umount)
+          COMPREPLY=($(compgen -W "N" -- ${cur} ))
+          ;;
+      esac
+      ;;
+    *)
+      COMPREPLY=()
+      ;;
+  esac
+
+}
+
+complete -F _mount_image_completion mount_image.sh
+
